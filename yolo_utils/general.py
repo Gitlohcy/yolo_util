bc20765ff407c9f54034e8561eeb0dd180848ad7
lohcy <loh.cheeyung96@gmail.com>
2021-01-30 12:22:39 +0800

    df_display_beside()

--- yolo_utils/general.py
+++ yolo_utils/general.py
@@ -59,5 +59,21 @@ def load_yaml(fpath):
         dict_ = yaml.load(f, Loader=yaml.FullLoader)
         return dict_
 
+#pd dataframe
+def df_display_beside(dfs, names=[]):
+    html_str = ''
+    if names:
+        html_str += ('<tr>' + 
+                     ''.join(f'<td style="text-align:center">{name}</td>' for name in names) + 
+                     '</tr>')
+    html_str += ('<tr>' + 
+                 ''.join(f'<td style="vertical-align:top"> {df.to_html(index=False)}</td>' 
+                         for df in dfs) + 
+                 '</tr>')
+    html_str = f'<table>{html_str}</table>'
+    html_str = html_str.replace('table','table style="display:inline"')
+    display_html(html_str, raw=True)
+
+
 Path.ls = ls
 Path.lls = lls
